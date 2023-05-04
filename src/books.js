7fd0235a2c5859d71fdb4c833bafc15ab7c651b1
Filip <fpodsiadly@gmail.com>
2023-05-04 19:20:14 +0200

    Initial project - start

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/book1.jpg';
+import img2 from './images/book2.jpg';
+import img3 from './images/book3.jpg';
+
+export const books = [
+  {
+    author: 'Emily Henry',
+    title: 'Happy Place',
+    img: img1,
+    id: 1,
+  },
+
+  {
+    author: 'Suzanne Collins',
+    title: 'The Ballad of Songbirds and Snakes',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Luke Russert',
+    title: 'Look for Me There: Grieving My Father, Finding Myself',
+    img: img3,
+    id: 2,
+  },
+];
